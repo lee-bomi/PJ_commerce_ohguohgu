56d160842e4f30b7d50ffa4a93d0c5d3ab73835f
emmababy <bomvll@naver.com>
2022-12-25 12:27:00 +0900

    Project setting

---
+++ src/main/java/com/emma_dev/ohguohgu/OhguohguApplication.java
@@ -0,0 +1,13 @@
+package com.emma_dev.ohguohgu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhguohguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhguohguApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emma_dev/ohguohgu/OhguohguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emma_dev.ohguohgu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhguohguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
