46e42ff920bb13495693b5f229b835df930073b4
emmababy <bomvll@naver.com>
2022-12-27 12:45:26 +0900

    feat: Project setting
    DB 연결, 타임리프 설정 추가하여 기본동작 확인

--- src/main/java/com/emma_dev/ohguohgu/Main.java
+++
@@ -1,7 +0,0 @@
-package com.emma_dev.ohguohgu;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/emma_dev/ohguohgu/MainController.java
@@ -0,0 +1,15 @@
+package com.emma_dev.ohguohgu;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "/index";
+    }
+}
