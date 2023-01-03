0d5bbb6d7b5b13804214bc98dd60dd44e8b3a8ed
emmababy <bomvll@naver.com>
2023-01-03 20:01:03 +0900

    refactor : 권한별로 index페이지 분리

--- src/main/java/com/emma_dev/ohguohgu/MainController.java
+++ src/main/java/com/emma_dev/ohguohgu/MainController.java
@@ -1,18 +1,26 @@
 package com.emma_dev.ohguohgu;
 
+import com.emma_dev.ohguohgu.admin.entity.Item;
+import com.emma_dev.ohguohgu.admin.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
 
-    @GetMapping("/")
-    public String main() {
+    private final AdminService adminService;
 
+    @GetMapping("/")
+    public String main(Model model) {
+
+        List<Item> itemList = adminService.getItemList();
+        model.addAttribute("itemList", itemList);
 
         return "/index";
     }
